7b52f19fd0bf64c82cf1a5e16824291ef91e9cc6
antonioneto2 <antonio.nog28@gmail.com>
2024-06-26 14:58:57 -0300

    finalizando projeto entregavel

--- src/script.js
+++ src/script.js
@@ -1,11 +1,42 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(numero1, numero2 , operador){
+  let resultado
+  let num1 = Number(numero1)
+  let num2 = Number(numero2)
 
+  switch(operador){
+    case "+":
+      resultado = num1 + num2
+      break;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    case "-":
+      resultado = num1 - num2
+      break;
+     
+    case "*":
+      resultado = num1 * num2
+      break; 
+      
+    case "/":
+      resultado = num1 / num2
+      break;
+    
+    case "e":
+      let contador = 0
+      resultado = 1
+      while (contador < num2 ) {
+        resultado = resultado *num1
+        contador++
+      }
+      break;
+  }
+
+  if(resultado == undefined || resultado > 1000000)
+    resultado = "ERRO"
+
+  return resultado
+}
+
 
 let visor = document.getElementById("visor");
 
